6b914b3ad0c9980462b63f05b1586b50bd9843e8
Reda BELHAJ <reda.belhaajj@gmail.com>
2021-07-28 14:45:06 +0100

    Update main.go

--- main.go
+++ main.go
@@ -1,5 +1,5 @@
 /*
-Copyright © 2021 NAME HERE Reda BELHAJ
+Copyright © 2021 Reda BELHAJ
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
